b1502729e3cc5d31bc4d3a4ac473db2522501303
Dan Stroud <dstroud@mac.com>
2023-05-31 20:12:34 -0500

    update includes doc

--- lib/includes.lua
+++ lib/includes.lua
@@ -3,9 +3,13 @@ ER = require("er")
 musicutil = require("musicutil")
 engine.name = "PolyPerc"
 
-
 local lib = "dreamsequence/lib/"
-
 
 -- divisions for clock modulo and durations
 include(lib .. "divisions")
+
+-- Lookup table for events
+include(lib .. "events")
+
+-- Chord and arp pattern generator + engine params
+include(lib .. "generator")
